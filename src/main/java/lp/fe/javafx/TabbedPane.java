01b1532db99c4ceada6198353cf0d0e7e1310543
Lubor Pešek <lubor.pesek@cleverlance.com>
2023-09-03 23:53:41 +0200

    refactoring - first selected tab index

--- src/main/java/lp/fe/javafx/TabbedPane.java
+++ src/main/java/lp/fe/javafx/TabbedPane.java
@@ -30,7 +30,6 @@ public class TabbedPane {
             tabPane.getTabs().add(bf2Components.get(observable.getFrom()).getTab());
         });
         fillTabs();
-        tabPane.getSelectionModel().selectLast();
     }
 
     public void resize(Stage stage) {
